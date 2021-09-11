99eff9192d2d6531cb455ed4ff0c2943c94b059a
Corey Kunz <cnkunz@gmail.com>
2021-09-11 09:17:29 -0400

    install flask

--- code/app.py
+++ code/app.py
@@ -1,13 +1,37 @@
 from sqlalchemy import create_engine
 
-mysql_conn_str = "mysql+pymysql://root:dev@db:3306/product"
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as ps
+import ast
 
-engine = create_engine(mysql_conn_str)
+app = Flask(__name__)
+api = Api(app)
 
-connection = engine.connect()
+class Orders(Resource):
+    # methods go here
+    pass
 
-q = connection.execute('SHOW DATABASES')
+class Product(Resource):
+    def get(self):
+        data = GetProducts()
+        data = data.to_dict()
+        return {'data': data }, 200
 
-available_tables = q.fetchall()
+api.add_resource(Orders, '/orders')  # '/users' is our entry point for Users
+api.add_resource(Product, '/product')  # and '/locations' is our entry point for Locations
 
-print(available_tables)
+if __name__ == '__main__':
+    app.run()
+
+
+
+def GetProducts():
+    mysql_conn_str = "mysql+pymysql://root:dev@db:3306/product"
+    engine = create_engine(mysql_conn_str)
+    connection = engine.connect()
+    q = connection.execute('SHOW DATABASES')
+    available_tables = q.fetchall()
+
+
+    return available_tables
