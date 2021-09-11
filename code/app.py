29216d54f67fe36c2331db06ae0243ab650ebda2
Corey Kunz <cnkunz@gmail.com>
2021-09-11 11:29:24 -0400

    app update

--- code/app.py
+++ code/app.py
@@ -1,13 +1,48 @@
-from sqlalchemy import create_engine
-
 from flask import Flask
+from flask import jsonify
 from flask_restful import Resource, Api, reqparse
-import pandas as ps
-import ast
+import mysql.connector
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
+## MySQL connection ##
+conn = mysql.connector.connect(user='root', password='dev', host='db', database='product')
+
+if conn:
+    print('works')
+else:
+    print('fuck')
+
+class create_dict(dict):
+
+    # __init__ function
+    def __init__(self):
+        self = dict()
+
+    # Function to add key:value
+    def add(self, key, value):
+        self[key] = value
+
+
+def GetProduct(query):
+    mydict = create_dict()
+    select_stuff = query
+    cursor = conn.cursor()
+    cursor.execute(select_stuff)
+    result = cursor.fetchall()
+
+    for row in result:
+        mydict.add(row[0],({"item":row[0],"stock":row[1],"sold":row[2]}))
+
+    stud_json = jsonify(mydict)
+
+    return stud_json
+
+
+
+## Endpoints ##
 class Orders(Resource):
     # methods go here
     pass
@@ -16,17 +51,13 @@ class Product(Resource):
 
     def get(self):
 
-        mysql_conn_str = "mysql+pymysql://root:dev@db:3306/product"
-        engine = create_engine(mysql_conn_str)
-        connection = engine.connect()
-        q = connection.execute('SHOW DATABASES')
-        available_tables = q.fetchall()
-
-        data = available_tables
-        return {'data': data }, 200
+        data = GetProduct("""SELECT * FROM laptops""")
+        return data
+#        return {'data': data }, 200
 
 api.add_resource(Orders, '/orders')
-api.add_resource(Product, '/product') 
+api.add_resource(Product, '/product')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
