f5f46908f84d6acfe0bd0a0343985f5430e661fd
Corey Kunz <cnkunz@gmail.com>
2021-09-10 22:18:56 -0400

    maria dockerfile and app update

--- code/app.py
+++ code/app.py
@@ -1,27 +1,13 @@
-# We gonna do dis
-import sqlalchemy as db
+from sqlalchemy import create_engine
 
-# specify database configurations
-config = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'user',
-    'password': 'password',
-    'database': 'product'
-}
-db_user = config.get('user')
-db_pwd = config.get('password')
-db_host = config.get('host')
-db_port = config.get('port')
-db_name = config.get('database')
-# specify connection string
-connection_str = 'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
-# connect to database
-engine = db.create_engine(connection_str)
+mysql_conn_str = "mysql+pymysql://root:dev@web_app_db_1:3306/product"
+
+engine = create_engine(mysql_conn_str)
+
 connection = engine.connect()
-# pull metadata of a table
-metadata = db.MetaData(bind=engine)
-metadata.reflect(only=['laptops'])
 
-test_table = metadata.tables['laptops']
-test_table
+q = connection.execute('SHOW DATABASES')
+
+available_tables = q.fetchall()
+
+print(available_tables)
